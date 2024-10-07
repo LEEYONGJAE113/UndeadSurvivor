73e7ef1e717f4ffd6dfb83d7f6defd28fb150f06
DESKTOP-SCTFQSJ\A <ionsi1103@gmail.com>
2024-10-07 19:40:43 +0900

    [Update] 맵이동까지 함

--- Assets/Undead Survivor/Scripts/Player.cs
+++ Assets/Undead Survivor/Scripts/Player.cs
@@ -5,7 +5,7 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    private Vector2 _inputVec;
+    public Vector2 inputVec;
     [SerializeField]
     private float _speed;
 
@@ -24,33 +24,33 @@ public class Player : MonoBehaviour
 
     // void Update()
     // {
-    //     _inputVec.x = Input.GetAxisRaw("Horizontal"); // project settings - input manager - axis
-    //     _inputVec.y = Input.GetAxisRaw("Vertical"); // Raw를 빼면 미끄러지듯이 나감
+    //     inputVec.x = Input.GetAxisRaw("Horizontal"); // project settings - input manager - axis
+    //     inputVec.y = Input.GetAxisRaw("Vertical"); // Raw를 빼면 미끄러지듯이 나감
     // }
 
     void FixedUpdate() // 물리연산 프레임마다 호출
     {
-        Vector2 nextVec = _inputVec/*.normalized*/ * _speed * Time.fixedDeltaTime; // fixedDeltaTime은 FixedUpdate기준으로 동작
+        Vector2 nextVec = inputVec/*.normalized*/ * _speed * Time.fixedDeltaTime; // fixedDeltaTime은 FixedUpdate기준으로 동작
         // // 1. 힘을 준다
-        // _rigid.AddForce(_inputVec);
+        // _rigid.AddForce(inputVec);
         // // 2. 속도 제어
-        // _rigid.velocity = _inputVec;
+        // _rigid.velocity = inputVec;
         // 3. 위치 이동
         _rigid.MovePosition(_rigid.position + nextVec); // 현재 위치를 더해줘야함
     }
 
     void OnMove(InputValue value)
     {
-        _inputVec = value.Get<Vector2>();
+        inputVec = value.Get<Vector2>();
     }
 
     void LateUpdate() // 후처리, 프레임이 종료되기전 처리
     {
-        _anim.SetFloat("Speed", _inputVec.magnitude); // magnitude : 벡터의 순수 크기
+        _anim.SetFloat("Speed", inputVec.magnitude); // magnitude : 벡터의 순수 크기
 
-        if (_inputVec.x != 0)
+        if (inputVec.x != 0)
         {
-            _spriter.flipX = (_inputVec.x < 0); // bool값을 반환해서 flipX를 체크할지 말지
+            _spriter.flipX = (inputVec.x < 0); // bool값을 반환해서 flipX를 체크할지 말지
         }
     }
 }
