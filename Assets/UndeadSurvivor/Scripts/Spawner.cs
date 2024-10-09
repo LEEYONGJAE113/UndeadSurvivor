df80e73b1264f981e6afb3d05fed42017e7d877f
DESKTOP-SCTFQSJ\A <ionsi1103@gmail.com>
2024-10-09 20:26:19 +0900

    [Update] 소환레벨적용까지함..
    vscode 닷넷 자꾸 설치시도하는거 열받음

--- Assets/UndeadSurvivor/Scripts/Spawner.cs
+++ Assets/UndeadSurvivor/Scripts/Spawner.cs
@@ -5,33 +5,41 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public Transform[] spawnPoint;
+    public SpawnData[] spawnDatas;
 
+    int level;
     float timer; // 몹소환 타이머
 
     void Awake()
     {
         spawnPoint = GetComponentsInChildren<Transform>();
     }
+
     void Update()
     {
         timer += Time.deltaTime;
+        level = Mathf.FloorToInt(GameManager.instance.gameTime / 10f);
 
-        if (timer > 0.2f)
+        if (timer > (spawnDatas[level].spawnTime))
         {
             Spawn();
             timer = 0;
-        }
-
-        if (Input.GetButtonDown("Jump"))
-        {
-            GameManager.instance.pool.Get(1);
         }
     }
 
     void Spawn()
     {
-        GameObject enemy = GameManager.instance.pool.Get(Random.Range(0, 2));
+        GameObject enemy = GameManager.instance.pool.Get(0);
         enemy.transform.position = spawnPoint[Random.Range(1, spawnPoint.Length)].position; // children에 자기 자신도 포함돼서 1부터 시작
-
+        enemy.GetComponent<Enemy>().Init(spawnDatas[level]); 
     }
+}
+
+[System.Serializable] // 직렬화
+public class SpawnData
+{
+    public float spawnTime;
+    public int spriteType;
+    public int health;
+    public float speed;
 }
